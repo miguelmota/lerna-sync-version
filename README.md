299386a74cc315383dc63dc6bc5109984a41c2b5
Miguel Mota <miguelmota2@gmail.com>
2020-01-05 11:39:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# lerna-sync-version
+
+> Sync version of lerna packages locally.
+
+[![License](http://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/miguelmota/lerna-sync-version/master/LICENSE)
+[![NPM version](https://badge.fury.io/js/lerna-sync-version.svg)](http://badge.fury.io/js/lerna-sync-version)
+
+## Install
+
+```bash
+npm install -g lerna-sync-version
+```
+
+## Usage
+
+```bash
+$ lerna_sync_version [lerna-package-name]
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,88 @@
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const findUp = require('find-up')
+
+let syncPackageName = process.argv[2]
+
+if (!syncPackageName) {
+  try {
+    const closestPackage = findUp.sync('package.json')
+    if (!closestPackage) {
+      console.log(chalk.red('no lerna package was found'))
+      process.exit(1)
+    }
+
+    const closestPackageJson = require(closestPackage)
+    if (closestPackageJson.name) {
+      syncPackageName = closestPackageJson.name
+    } else {
+      console.log('Missing package name as argument')
+      process.exit(1)
+    }
+  } catch (err) {
+    console.log(chalk.red('could not read lerna package'))
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+const lernaPath = findUp.sync('lerna.json')
+
+if (!lernaPath) {
+  console.log(chalk.red('lerna project not found'))
+  process.exit(1)
+}
+
+const rootPath = path.dirname(lernaPath)
+
+let syncPackageJson = null
+const packagesDir = path.resolve(rootPath, 'packages')
+
+try {
+  const pkgDirname = syncPackageName.replace(/@[0-9a-zA-Z]+\//, '')
+  syncPackageJson = require(path.resolve(packagesDir, pkgDirname, 'package.json'))
+} catch (err) {
+  console.log(chalk.red(`package "${syncPackageName}" was not found`))
+  console.error(err)
+  process.exit(1)
+}
+
+const newVersion = syncPackageJson.version
+
+if (!newVersion) {
+  console.log(chalk.red(`package "${syncPackageName}" is missing "version"`))
+  process.exit(1)
+}
+
+console.log(`${syncPackageName}: ${newVersion}\n`)
+
+const getDirectories = source => {
+  return fs.readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+}
+
+const packages = getDirectories(packagesDir)
+packages.forEach(pkg => {
+  const filepath = path.resolve(packagesDir, pkg, 'package.json')
+  const stat = fs.statSync(filepath)
+  if (!stat.isFile) {
+    return
+  }
+
+  const packageJson = require(filepath)
+  if (!packageJson.dependencies[syncPackageName]) {
+    return
+  }
+
+  const oldVersion = packageJson.dependencies[syncPackageName]
+  if (oldVersion === newVersion) {
+    console.log(chalk.yellow(`not modified pkg ${pkg}:`.padEnd(30, '.'), `${oldVersion}`))
+    return
+  }
+
+  packageJson.dependencies[syncPackageName] = newVersion
+  fs.writeFileSync(filepath, JSON.stringify(packageJson, null, 2))
+  console.log(chalk.green(`UPDATED pkg ${pkg}:`.padEnd(30, '.'), `${oldVersion} => ${newVersion}`))
+})
